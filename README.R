9a626bd37408f8abde014599a7301a73f77b14af
smacrop <77694423+smacrop@users.noreply.github.com>
2023-06-24 22:35:24 -0400

    Delete README.R

--- README.R
+++
@@ -1,6 +0,0 @@
-# Code to render the README.md file -- Stephen Macropoulos
-
-library(rmarkdown)
-rmarkdown::render("C:/Users/Owner/OneDrive/Documents/ST501/ST558_Project1/README.Rmd",
-                  output_format = "github_document",
-                  output_dir = "C:/Users/Owner/OneDrive/Documents/ST501/ST558_Project1/Vignette/")

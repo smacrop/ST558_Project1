ef6db2dbb3e3180fe282987d75bba6f58fd237d7
Stephen Macropoulos <smacrop@ncsu.edu>
2023-06-24 22:39:42 -0400

    commit .R file 3

--- Vignette/README.R
+++ Vignette/README.R
@@ -3,4 +3,4 @@
 library(rmarkdown)
 rmarkdown::render("C:/Users/Owner/OneDrive/Documents/ST501/ST558_Project1/README.Rmd",
                   output_format = "github_document",
-                  output_dir = "C:/Users/Owner/OneDrive/Documents/ST501/ST558_Project1/Vignette1/")
+                  output_dir = "C:/Users/Owner/OneDrive/Documents/ST501/ST558_Project1/Vignette/")
